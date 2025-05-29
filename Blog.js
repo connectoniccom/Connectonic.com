dcc657ff89f6ed73a29827a2d3de682811cd068d
connectoniccom <Connectonic.com@gmail.com>
2025-05-29 18:19:10 +0300

    Update Blog.js

--- Blog.js
+++ Blog.js
@@ -1,3 +1,9 @@
+
+// Disable right-click
+document.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+}, false);
+
 // my loader
 setTimeout(() => {
     document.querySelector('.loader').style.display = 'none';
