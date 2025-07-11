fe47109a0df3d42a45f5c8865f6d54394139966d
Byabasajja Matayo <matayobyabasajja77@gmail.com>
2025-07-11 19:28:20 +0000

    Hello I want dark theme to be in light dark remove that button which is

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './Sidebar.css';
 
-function Sidebar({ isOpen, toggleTheme, currentTheme, closeSidebar }) {
+function Sidebar() {
   return (
-    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-      <a href="#home" onClick={closeSidebar}>Home</a>
-      <a href="#services" onClick={closeSidebar}>Services</a>
-      <a href="#clients" onClick={closeSidebar}>Clients</a>
-      <a href="#contact" onClick={closeSidebar}>Contact</a>
-      <button onClick={toggleTheme} className="theme-toggle-button">
-        Switch to {currentTheme === 'light' ? 'Dark' : 'Light'} Mode
-      </button>
+    <div className="sidebar">
+      <a href="#home">Home</a>
+      <a href="#services">Services</a>
+      <a href="#clients">Clients</a>
+      <a href="#contact">Contact</a>
     </div>
   );
 }
