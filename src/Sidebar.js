4755defe5b1cccda37fc96e9f00c4f36bb9ef880
Byabasajja Matayo <matayobyabasajja77@gmail.com>
2025-07-11 19:21:48 +0000

    Remove that dark box and make my web app to display full screen with lig

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Sidebar.css';
 
-function Sidebar({ isOpen }) {
+function Sidebar({ isOpen, toggleTheme, currentTheme }) {
   return (
     <div className={`sidebar ${isOpen ? 'open' : ''}`}>
       <a href="#home">Home</a>
       <a href="#services">Services</a>
       <a href="#clients">Clients</a>
       <a href="#contact">Contact</a>
+      <button onClick={toggleTheme} className="theme-toggle-button">
+        Switch to {currentTheme === 'light' ? 'Dark' : 'Light'} Mode
+      </button>
     </div>
   );
 }
