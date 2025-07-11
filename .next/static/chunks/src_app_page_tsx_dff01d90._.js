0e045449282942e4719792e57733bc5d0cf1b949
Byabasajja Matayo <matayobyabasajja77@gmail.com>
2025-07-11 06:08:57 +0000

    Changes to be committed:

---
+++ .next/static/chunks/src_app_page_tsx_dff01d90._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_c5390cca._.js",
+  "static/chunks/src_1911df1b._.js"
+],
+    source: "dynamic"
+});
