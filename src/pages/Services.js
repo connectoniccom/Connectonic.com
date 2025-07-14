0b4b83c929563209dd51297ce08669ceb952f47b
Byabasajja Matayo <matayobyabasajja77@gmail.com>
2025-07-14 19:17:27 +0000

    hello i want this web app to show to any device

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import './Services.css';
 
 function Services() {
   return (
     <section id="services" className="content-section">
-      <h2>Our Services</h2>
-      <p>We offer a wide range of services to meet your needs. Our team is dedicated to providing the highest quality solutions.</p>
-      <ul>
-        <li>Web Development</li>
-        <li>Mobile App Development</li>
-        <li>Cloud Solutions</li>
-        <li>UI/UX Design</li>
-      </ul>
+      <div className="services-container">
+        <h2>Our Services</h2>
+        <p>We offer a wide range of services to meet your needs. Our team is dedicated to providing the highest quality solutions.</p>
+        <ul className="services-list">
+          <li>Web Development</li>
+          <li>Mobile App Development</li>
+          <li>Cloud Solutions</li>
+          <li>UI/UX Design</li>
+        </ul>
+      </div>
     </section>
   );
 }
