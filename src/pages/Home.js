361d272127b24251dda8bbd96306e08e8e41ffcf
Byabasajja Matayo <matayobyabasajja77@gmail.com>
2025-07-16 13:59:51 +0000

    Add more ads display auto video ads flash ads add all types of ads you n

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,18 @@ function Home() {
     <section id="home" className="content-section">
       <h1>Welcome Home</h1>
       <p>This is the main landing area of our application. Explore the different sections using the sidebar.</p>
+      
+      {/* First Ad Slot */}
+      <div style={{ margin: '40px 0' }}>
+        <Ad adSlot="1234567890" />
+      </div>
+
       <img src="https://placehold.co/600x400.png" alt="placeholder" data-ai-hint="futuristic city" />
 
-      {/* Example Ad */}
+      {/* Second Ad Slot */}
       <div style={{ margin: '40px 0' }}>
-        <Ad />
+        <p>A second ad could appear here.</p>
+        <Ad adSlot="0987654321" />
       </div>
     </section>
   );
