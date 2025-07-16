4d4fe598b678049b110fb14670604b10ad69f564
Byabasajja Matayo <matayobyabasajja77@gmail.com>
2025-07-16 13:54:13 +0000

    The `npm install` command failed in my project. Analyze the following er

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Ad from '../Ad';
 
 function Home() {
   return (
@@ -6,6 +7,11 @@ function Home() {
       <h1>Welcome Home</h1>
       <p>This is the main landing area of our application. Explore the different sections using the sidebar.</p>
       <img src="https://placehold.co/600x400.png" alt="placeholder" data-ai-hint="futuristic city" />
+
+      {/* Example Ad */}
+      <div style={{ margin: '40px 0' }}>
+        <Ad />
+      </div>
     </section>
   );
 }
