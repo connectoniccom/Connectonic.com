e8e2d43921b1082785de0508f9e0ff3294e4621b
Byabasajja Matayo <matayobyabasajja77@gmail.com>
2025-07-16 06:21:19 +0000

    No I'm asking is this web app or what

---
+++ public/service-worker.js
@@ -0,0 +1,4 @@
+// This service worker file is intentionally left empty.
+// It is a placeholder file that will be automatically
+// replaced by the build process with the actual service worker code.
+// Do not add any code to this file.
