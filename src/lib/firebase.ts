b8cba2a492f2181d8a1d1a801b0f8cad33036c93
Byabasajja Matayo <matayobyabasajja77@gmail.com>
2025-07-23 17:53:22 +0000

    All

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -7,7 +10,7 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   "projectId": "connectoniccom-k2gsh",
   "appId": "1:370207863142:web:8dec55f0969798e9f098a7",
-  "storageBucket": "connectoniccom-k2gsh.firebasestorage.app",
+  "storageBucket": "connectoniccom-k2gsh.appspot.com",
   "apiKey": "AIzaSyA6kP59kWoPLKedD9C2XIcvPHUdJQv4oE0",
   "authDomain": "connectoniccom-k2gsh.firebaseapp.com",
   "measurementId": "",
@@ -16,3 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
