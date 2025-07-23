004439a79f199f718fc76d181759171dbcf3b84e
Byabasajja Matayo <matayobyabasajja77@gmail.com>
2025-07-23 17:48:42 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "connectoniccom-k2gsh",
+  "appId": "1:370207863142:web:8dec55f0969798e9f098a7",
+  "storageBucket": "connectoniccom-k2gsh.firebasestorage.app",
+  "apiKey": "AIzaSyA6kP59kWoPLKedD9C2XIcvPHUdJQv4oE0",
+  "authDomain": "connectoniccom-k2gsh.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "370207863142"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
