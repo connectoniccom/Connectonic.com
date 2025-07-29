ca7bdcb7c9a5713d1e9cf6e1d9f7b22947758030
Byabasajja Matayo <matayobyabasajja77@gmail.com>
2025-07-29 16:33:10 +0000

    Hello you have forgotten my lock.js file because it also have some funct

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const page = window.location.pathname;
     let scriptToLoad;
+    let lockScript = 'lock.js';
 
     if (page.includes('Card.html')) {
         scriptToLoad = 'Card.js';
@@ -8,6 +9,12 @@ document.addEventListener('DOMContentLoaded', () => {
         scriptToLoad = 'script.js';
     }
 
+    if (lockScript) {
+        const script = document.createElement('script');
+        script.src = lockScript;
+        document.head.appendChild(script);
+    }
+    
     if (scriptToLoad) {
         const script = document.createElement('script');
         script.src = scriptToLoad;
