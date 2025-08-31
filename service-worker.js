c7431bc08dd12b7432066c1d88fac3f3d717843c
connectoniccom <Connectonic.com@gmail.com>
2025-08-31 19:25:16 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-// This service worker file is intentionally left empty.
-// It is a placeholder file that will be automatically
-// replaced by the build process with the actual service worker code.
-// Do not add any code to this file.
