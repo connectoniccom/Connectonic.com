ba3380d68a5dbd5e2a723c645f42b7c3f6419afe
Byabasajja Matayo <matayobyabasajja77@gmail.com>
2025-09-02 10:50:14 +0000

    Hello in artists page change DJ echo to David Lutalo and use this link a

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "433f200bad775b6a"; }
+/******/ 		__webpack_require__.h = function() { return "1d5448591aa1a033"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
